8f1a2873fa03f5ce9f8a23c8e96fca3f735de98c
Håkan Johansson <ninjaharry@gmail.com>
2023-12-01 12:57:01 +0100

    Added check for username already exists. EXTERMINATE

--- functions/signUp/index.js
+++ functions/signUp/index.js
@@ -3,9 +3,27 @@ const { nanoid } = require("nanoid");
 const bcrypt = require('bcryptjs');
 const AWS = require('aws-sdk');
 const db = new AWS.DynamoDB.DocumentClient();
+const dynaDB = new AWS.DynamoDB();
 
 async function createAccount(username, hashedPassword, firstname, lastname) {
   try {
+    //check if username already exists
+    const items = await db.query({
+      TableName: 'accounts',
+      KeyConditionExpression: '#username = :username',
+      ExpressionAttributeValues: {
+        ":username" : username
+      },
+      ExpressionAttributeNames: {
+        "#username": "username"
+      }
+    }).promise();
+
+    if(items.Items.length > 0) {
+      return {success: false, message: "Username already exists."};
+    }
+
+    //Save user
     await db.put({
       TableName: 'accounts',
       Item: {
@@ -24,19 +42,21 @@ async function createAccount(username, hashedPassword, firstname, lastname) {
 }
 
 async function signup(username, password, firstname, lastname) {
-  //Check if username alread exists
-  //If true return { success: false, message: 'Username already exists'}
-  
   const hashedPassword = await bcrypt.hash(password, 10);
   
   return await createAccount(username, hashedPassword, firstname, lastname);
 }
 
 exports.handler = async (event) => {
-  const {username, password, firstname, lastname} = JSON.parse(event.body);
+  let result;
 
-  // signup
-  const result = await signup(username, password, firstname, lastname);
+  try {
+    const {username, password, firstname, lastname} = JSON.parse(event.body);
+    // signup
+    result = await signup(username, password, firstname, lastname);
+  } catch(error) {
+    return sendResponse(400, {success: false, message: "Input not correct. EXTERMINATE"});
+  }
 
   if(result.success) {
    return sendResponse(200, result) 
